f10b011542bca82876479ebd4eb45f1ac8ccff94
dlacorte10 <daniel.lacorte10@gmail.com>
2022-01-15 17:17:08 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -37,10 +37,14 @@ function Listing() {
         score: 4.5
     };
 
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
 
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
+
             <div className="container">
                 <div className="row">
                     {page.content.map(movie => (
